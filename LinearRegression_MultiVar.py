f7b5d85e09981d28da1611a4d6c3c8e5b86a0889
Rajpal <rajpal.kulhari14@gmail.com>
2014-08-12 23:56:59 +0530

    Update LinearRegression_MultiVar.py

--- LinearRegression_MultiVar.py
+++ LinearRegression_MultiVar.py
@@ -1,4 +1,4 @@
-​from numpy import loadtxt, zeros, ones, array, linspace, logspace, mean, std, arange
+from numpy import loadtxt, zeros, ones, array, linspace, logspace, mean, std, arange
 from pylab import plot, show, xlabel, ylabel
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -26,6 +26,8 @@ def featureNormalize(X):
 
 def gradientDescent(X, y, theta, alpha, num_iters):
         m = y.size
+	print X
+    	#J_history = zeros(shape=(num_iters, 1))
     	for i in range(num_iters):
         	predictions = X.dot(theta)
         	theta_size = theta.size
@@ -34,6 +36,7 @@ def gradientDescent(X, y, theta, alpha, num_iters):
             		temp.shape = (m, 1)
             		errors_x1 = (predictions - y) * temp
             		theta[it][0] = theta[it][0] - alpha * (1.0 / m) * errors_x1.sum()
+        	#J_history[i, 0] = compute_cost(X, y, theta)
     	return theta
         
 def gradientDescentUtil(X, y, theta):
@@ -55,21 +58,8 @@ def plotInputData(data):
 
     ax.set_xlabel('Size of the House')
     ax.set_ylabel('Number of Bedrooms')
-    ax.set_zlabel('Price of the House')
+    ax.set_zlabel('House Price')
     plt.show()
-    
-def featureManipulation(data):
-    #feature extractions
-    X = data[:, :2]
-    y = data[:, 2]
-    #number of training samples
-    m = y.size
-    #convert y to a vector
-    y.shape = (m, 1)
-    #Add a column of ones to X (interception data)
-    it = ones(shape=(m, 3))
-    it[:, 1:3] = X
-    return it,y
 
 def predictValues(theta,mean_r,std_r,size,bhk):
         price = array([1.0,   ((size - mean_r[0]) / std_r[0]), ((bhk - mean_r[1]) / std_r[1])]).dot(theta)
@@ -80,22 +70,31 @@ def main():
         data = loadtxt('LinearRegression_MultiVar_input.txt', delimiter=',')
         plotInputData(data)
 
-        #feature manipulation
-        X,y = featureManipulation(data)
-	
-        #Scale features and calculate mean and std_deviation
-	temp = data[:, :2]
-        x, mean_r, std_r = featureNormalize(temp)
+        #feature extractions
+	X = data[:, :2]
+	y = data[:, 2]
+
+	#number of training samples
+	m = y.size
+
+	#convert y to a vector
+	y.shape = (m, 1)
+
+	#Scale features and calculate mean and std_deviation
+	x, mean_r, std_r = featureNormalize(X)
+
+	#Add a column of ones to X (interception data)
+	it = ones(shape=(m, 3))
+	it[:, 1:3] = X
 
         #Initialize theta parameters
         theta = zeros(shape=(3, 1))
 
         #call gradientDescent
-        theta = gradientDescentUtil(X, y, theta)
+        theta = gradientDescentUtil(it, y, theta)
 
         #Predict values 
         predictValues(theta,mean_r,std_r,1650.0,3)
         
 if __name__ == '__main__':
     main()
-​
